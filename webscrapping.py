9b1d7d3725c27bd4907861d3c4d420841643391b
PedroSoteloB <113255967+PedroSoteloB@users.noreply.github.com>
2024-04-19 13:03:35 -0500

    Update webscrapping.py

--- webscrapping.py
+++ webscrapping.py
@@ -5,16 +5,13 @@ import time
 
 app = Flask(__name__)
 
-# Control del número máximo de llamadas por minuto
 MAX_CALLS_PER_MINUTE = 20
 calls_made = 0
 last_call_time = 0
 
-# Función para buscar la entidad en la fuente específica mediante web scraping con Selenium
 def search_entity_in_source(entity_name):
     global calls_made, last_call_time
 
-    # Verificar el número máximo de llamadas por minuto
     current_time = time.time()
     if current_time - last_call_time < 60:
         calls_made += 1
@@ -24,30 +21,22 @@ def search_entity_in_source(entity_name):
         calls_made = 1
         last_call_time = current_time
 
-    # Inicializar el driver del navegador (en este caso, Chrome)
     driver = webdriver.Chrome()
 
-    # Navegar a la página que quieres
     driver.get("https://projects.worldbank.org/en/projects-operations/procurement/debarred-firms")
 
-    # Esperar un poco para asegurarnos de que la página se cargue completamente
     time.sleep(5)
 
-    # Encontrar la tabla de resultados usando el XPath proporcionado
     table = driver.find_element(By.XPATH, '//*[@id="k-debarred-firms"]/div[3]/table')
 
-    # Encontrar todas las filas de la tabla de resultados
     rows = table.find_elements(By.XPATH, "./tbody/tr")
 
-    # Buscar la entidad en las filas de la tabla
     hits = 0
     results = []
     for row in rows:
-        # Buscar el nombre de la entidad en la primera columna (FIRM NAME)
         firm_name = row.find_element(By.XPATH, "./td[1]").text.strip()
         if entity_name.lower() in firm_name.lower():
             hits += 1
-            # Almacenar los detalles de la entidad
             entity_details = {
                 "FIRM NAME": firm_name,
                 "ADDRESS": row.find_element(By.XPATH, "./td[3]").text.strip(),
@@ -58,18 +47,14 @@ def search_entity_in_source(entity_name):
             }
             results.append(entity_details)
 
-    # Cerrar el driver
     driver.quit()
 
     return hits, results, ""
 
-# Ruta para realizar la búsqueda de la entidad
 @app.route('/search', methods=['GET'])
 def search_entity():
-    # Recibir el nombre de la entidad de los parámetros de la URL
     entity_name = request.args.get('entity_name')
 
-    # Realizar la búsqueda en la fuente específica mediante web scraping con Selenium
     hits, results, error_message = search_entity_in_source(entity_name)
 
     if error_message:
